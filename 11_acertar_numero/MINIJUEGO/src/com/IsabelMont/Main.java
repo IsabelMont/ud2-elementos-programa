19952f724b22262b2d63cfce5da90ba27fe20fbf
Unknown <isa.montero92@gmail.com>
2017-10-18 20:29:53 +0200

    AcertarNumerpo

---
+++ 11_acertar_numero/MINIJUEGO/src/com/IsabelMont/Main.java
@@ -0,0 +1,18 @@
+package com.IsabelMont;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int repeticiones = 0;
+
+	Random r = new Random();
+	int aleatorio = r.nextInt();
+
+
+    }
+}
